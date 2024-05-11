1cfb5a9c34f4e6962ff250e990141b02d33edc70
Nisadi Mithara <127333277+mithara2008@users.noreply.github.com>
2024-05-11 11:59:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/diaryapp/AddNoteActivity.kt
@@ -0,0 +1,11 @@
+package com.example.diaryapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_note)
+    }
+}
